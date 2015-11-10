2b8d89abc21d818c3655efba9d9a367d307815ab
proton5000 <proton5000@gmail.com>
2015-11-10 23:18:04 +0200

    sql data to database

---
+++ src/main/resources/dishes.sql
@@ -0,0 +1,32 @@
+-- Input of dishes
+-- есть вопрос по колонке types_of_dishes тип даннх INT,
+--  но вроде как решали что тип блюда будет стринга, пока оставил со стрингой
+INSERT INTO dishes(deadline,description,name,photo,price_new,price_original,types_of_dishes)
+VALUES ('2015-10-11','Легкая пицца с осенними овощами (шампиньоны, помидоры, болгарский перец, баклажаны, лук, ароматный чеснок, сыр пармезан и моцарелла','Грация','/resources/img/pizza.jpg','100','120','pizza');
+
+INSERT INTO dishes(deadline,description,name,photo,price_new,price_original,types_of_dishes)
+VALUES ('2015-10-12','Пицца-соус, сыр "Моцарелла", ветчина, ананасы. Экзотический вкус!','Гавайская','/resources/img/pizza.jpg','130','150','pizza');
+
+INSERT INTO dishes(deadline,description,name,photo,price_new,price_original,types_of_dishes)
+VALUES ('2015-10-11','Вегетарианское блюдо. Постное Морские маринованные водоросли.','Чука','/resources/img/sushi.jpg','10','16','sushi');
+
+INSERT INTO dishes(deadline,description,name,photo,price_new,price_original,types_of_dishes)
+VALUES ('2015-10-11','Морской окунь','Тай','/resources/img/sushi.jpg','28','32','sushi');
+
+INSERT INTO dishes(deadline,description,name,photo,price_new,price_original,types_of_dishes)
+VALUES ('2015-10-11','Бургер, с двумя говяжими котлетами, сыром да овощами, с майонезно-каперсовым соусом, в салатном листе обернутый.','Бургер "Ковбойский"','/resources/img/burgers.jpg','130','168','burgers');
+
+INSERT INTO dishes(deadline,description,name,photo,price_new,price_original,types_of_dishes)
+VALUES ('2015-10-11','Котлета из свинины и говядины, нежный сыр, маринованный лучок и соленый огурчик.','Чизбургер','/resources/img/burgers.jpg','80','96','burgers');
+
+INSERT INTO dishes(deadline,description,name,photo,price_new,price_original,types_of_dishes)
+VALUES ('2015-10-11','Пресноводный угорь.','Унаги','/resources/img/fish.jpg','18','32','fish');
+
+INSERT INTO dishes(deadline,description,name,photo,price_new,price_original,types_of_dishes)
+VALUES ('2015-10-11','Тунец','Магуро','/resources/img/fish.jpg','20','25','fish');
+
+INSERT INTO dishes(deadline,description,name,photo,price_new,price_original,types_of_dishes)
+VALUES ('2015-10-11','Деревенская свинина, подрумяненная на чугунной сковороде, с лучком и зеленью.','Свинина "Хуторская" на сковороде','/resources/img/chine.jpg','100','128','chine');
+
+INSERT INTO dishes(deadline,description,name,photo,price_new,price_original,types_of_dishes)
+VALUES ('2015-10-11','Отборная телячья вырезка, на углях сготовленная да облитая густой поливою "Джек Дениэлс".','Филей телячий "Джек Дэниэлс"е','/resources/img/chine.jpg','120','178','chine');
