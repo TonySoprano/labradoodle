7890ea64d4cc26a61c26de90cd5fdab176a04cc7
proton5000 <proton5000@gmail.com>
2015-11-11 15:01:14 +0200

    fix sql data to database

--- src/main/resources/restaurants.sql
+++ src/main/resources/restaurants.sql
@@ -1,9 +1,9 @@
 -- Input of dishes
 INSERT INTO restaurants(id,address,close_time,email,name,open_time,phone)
-VALUES ('1','Карла маркса 5','23:00','dvemarki@email.com','Две марки','10:00','+380667894561');
+VALUES ('1','DNKDNKLNN','23:00','dvemarki@email.com','Две марки','10:00','+380667894561');
 
 INSERT INTO restaurants(id,address,close_time,email,name,open_time,phone)
-VALUES ('2','Кирова 12','22:00','birhaus@email.com','Бирхаус','11:00','+380971234569');
+VALUES ('2','DNKDNKKMK','22:00','birhaus@email.com','Бирхаус','11:00','+380971234569');
 
 INSERT INTO restaurants(id,address,close_time,email,name,open_time,phone)
-VALUES ('3','Короленка 7','23:00','mafiya@email.com','Мафия','10:00','+380631597526');
+VALUES ('3','DNKDNKROG','23:00','mafiya@email.com','Мафия','10:00','+380631597526');
