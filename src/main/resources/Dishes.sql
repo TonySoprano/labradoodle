dc6cd9084cc87aafbfcd40e0a4d814a1524adad4
proton5000 <proton5000@gmail.com>
2015-12-04 11:47:07 +0200

    front: - add images by type; - add sort by type;

--- src/main/resources/Dishes.sql
+++ src/main/resources/Dishes.sql
@@ -1,30 +1,30 @@
 -- Input of dishes
 INSERT INTO dishes(dish_id,deadline,description,name,photo,price_new,price_original,types_of_dishes,restaurant_id)
-VALUES ('1','2015-10-11 22:31:20','Легкая пицца с осенними овощами (шампиньоны, помидоры, болгарский перец, баклажаны, лук, ароматный чеснок, сыр пармезан и моцарелла','Грация','/resources/img/pizza.jpg','100','120','0','1');
+VALUES ('1','2015-10-11 22:31:20','Легкая пицца с осенними овощами (шампиньоны, помидоры, болгарский перец, баклажаны, лук, ароматный чеснок, сыр пармезан и моцарелла','Грация','pizza.jpg','100','120','0','1');
 
 INSERT INTO dishes(dish_id,deadline,description,name,photo,price_new,price_original,types_of_dishes,restaurant_id)
-VALUES ('2','2015-10-12 23:12:12','Пицца-соус, сыр "Моцарелла", ветчина, ананасы. Экзотический вкус!','Гавайская','/resources/img/pizza.jpg','130','150','0','2');
+VALUES ('2','2015-10-12 23:12:12','Пицца-соус, сыр "Моцарелла", ветчина, ананасы. Экзотический вкус!','Гавайская','pizza.jpg','130','150','0','2');
 
 INSERT INTO dishes(dish_id,deadline,description,name,photo,price_new,price_original,types_of_dishes,restaurant_id)
-VALUES ('3','2015-10-11 12:31:25','Вегетарианское блюдо. Постное Морские маринованные водоросли.','Чука','/resources/img/sushi.jpg','10','16','4','1');
+VALUES ('3','2015-10-11 12:31:25','Вегетарианское блюдо. Постное Морские маринованные водоросли.','Чука','sushi.jpg','10','16','4','1');
 
 INSERT INTO dishes(dish_id,deadline,description,name,photo,price_new,price_original,types_of_dishes,restaurant_id)
-VALUES ('4','2015-10-11 11:45:35','Морской окунь','Тай','/resources/img/sushi.jpg','28','32','4','3');
+VALUES ('4','2015-10-11 11:45:35','Морской окунь','Тай','sushi.jpg','28','32','4','3');
 
 INSERT INTO dishes(dish_id,deadline,description,name,photo,price_new,price_original,types_of_dishes,restaurant_id)
-VALUES ('5','2015-10-11 23:11:18','Бургер, с двумя говяжими котлетами, сыром да овощами, с майонезно-каперсовым соусом, в салатном листе обернутый.','Бургер "Ковбойский"','/resources/img/burgers.jpg','130','168','2','2');
+VALUES ('5','2015-10-11 23:11:18','Бургер, с двумя говяжими котлетами, сыром да овощами, с майонезно-каперсовым соусом, в салатном листе обернутый.','Бургер "Ковбойский"','burgers.jpg','130','168','2','2');
 
 INSERT INTO dishes(dish_id,deadline,description,name,photo,price_new,price_original,types_of_dishes,restaurant_id)
-VALUES ('6','2015-10-11 20:20:20','Котлета из свинины и говядины, нежный сыр, маринованный лучок и соленый огурчик.','Чизбургер','/resources/img/burgers.jpg','80','96','2','3');
+VALUES ('6','2015-10-11 20:20:20','Котлета из свинины и говядины, нежный сыр, маринованный лучок и соленый огурчик.','Чизбургер','burgers.jpg','80','96','2','3');
 
 INSERT INTO dishes(dish_id,deadline,description,name,photo,price_new,price_original,types_of_dishes,restaurant_id)
-VALUES ('7','2015-10-11 20:20:25','Пресноводный угорь.','Унаги','/resources/img/fish.jpg','18','32','3','3');
+VALUES ('7','2015-10-11 20:20:25','Пресноводный угорь.','Унаги','fish.jpg','18','32','3','3');
 
 INSERT INTO dishes(dish_id,deadline,description,name,photo,price_new,price_original,types_of_dishes,restaurant_id)
-VALUES ('8','2015-10-11 18:19:20','Тунец','Магуро','/resources/img/fish.jpg','20','25','3','2');
+VALUES ('8','2015-10-11 18:19:20','Тунец','Магуро','fish.jpg','20','25','3','2');
 
 INSERT INTO dishes(dish_id,deadline,description,name,photo,price_new,price_original,types_of_dishes,restaurant_id)
-VALUES ('9','2015-10-11 17:18:19','Деревенская свинина, подрумяненная на чугунной сковороде, с лучком и зеленью.','Свинина "Хуторская" на сковороде','/resources/img/chine.jpg','100','128','1','1');
+VALUES ('9','2015-10-11 17:18:19','Деревенская свинина, подрумяненная на чугунной сковороде, с лучком и зеленью.','Свинина "Хуторская" на сковороде','chine.jpg','100','128','1','1');
 
 INSERT INTO dishes(dish_id,deadline,description,name,photo,price_new,price_original,types_of_dishes,restaurant_id)
-VALUES ('10','2015-10-11 20:25:35','Отборная телячья вырезка, на углях сготовленная да облитая густой поливою "Джек Дениэлс".','Филей телячий "Джек Дэниэлс"','/resources/img/chine.jpg','120','178','1','2');
+VALUES ('10','2015-10-11 20:25:35','Отборная телячья вырезка, на углях сготовленная да облитая густой поливою "Джек Дениэлс".','Филей телячий "Джек Дэниэлс"','chine.jpg','120','178','1','2');
