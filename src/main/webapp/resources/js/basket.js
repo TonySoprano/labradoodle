7605525f72322f2ba6d3f295f5a034f7a88ae207
a2driano <a2driano@gmail.com>
2015-12-16 22:07:37 +0200

    +start design new basket

--- src/main/webapp/resources/js/basket.js
+++ src/main/webapp/resources/js/basket.js
@@ -24,6 +24,44 @@ Events = function () {
                     var type = data[i];
                     var date = new Date(type.dishDto.deadline);
                     $('#HotDishesInside')
+                        .append('<a href="#' + type.dishDto.dishId + '"><div class="dishesBasket" index="' + type.dishDto.dishId + '"><div class="dishesBasketImage" style="background-image: url(../img/' + type.dishDto.photo + '); background-size: cover;"></div><div class="delBasket" title="Удалить из корзины"><div class="delBasketMinus"></div></div><div class="dishesinfo"><div class="dishesinfotextBasket">' + type.dishDto.name + '</div><div class="dishesinfoNEWPriceBasket">' + type.dishDto.priceNew + '</div></div></div></a>')
+                        .append('<div id="' + type.dishDto.dishId + '" class="modalDialog"><div><div class="modalImage" index="' + type.dishDto.dishId + '" style="background-image: url(../img/' + type.dishDto.photo + '); background-size: cover;"></div>' +
+                        '<div class="modalInfoHeader">' + type.name + '</div><div class="modaldeadline">' + date.getHours() + ':' + date.getMinutes() + '</div>' +
+                        '<div class="modalInfo"><p class="modalInfoText">' + type.dishDto.description + '</p></div>' +
+                        '<div class="modalOLDPrice">' + type.dishDto.priceOriginal + '</div><div class="modalNEWPrice">' + type.dishDto.priceNew + '</div>' +
+                        '<a href="#close" title="Закрыть" class="close">X</a></div></div>');
+                }
+            },
+            error: function (error) {
+                console.log(error)
+            }
+        });
+    });
+
+
+
+
+
+
+
+
+
+    //click by basket icon
+    $('.head').on('click', '.basket', function () {
+        $('#HotDishesInside').html('');
+        $('#HotDeals h1').text('BASKET');
+
+        $.ajax({
+            url: $hostRoot + 'basket/get/alldishes',
+            type: 'get',
+            dataType: 'json',
+            contentType: 'application/json',
+            success: function (data) {
+
+                for (var i = 0; i < data.length; i++) {
+                    var type = data[i];
+                    var date = new Date(type.dishDto.deadline);
+                    $('#HotDishesInside')
                         .append('<a href="#' + type.dishDto.dishId + '"><div class="dishes" index="' + type.dishDto.dishId + '" style="background-image: url(../img/' + type.dishDto.photo + '); background-size: cover;"><div class="deadline">' + date.getHours() + ':' + date.getMinutes() + '</div><div class="delBasket" title="Удалить из корзины"><div class="delBasketMinus"></div></div><div class="dishesinfo"><div class="dishesinfotext">' + type.dishDto.name + '</div><div class="dishesinfoOLDPrice">' + type.dishDto.priceOriginal + '</div><div class="dishesinfoNEWPrice">' + type.dishDto.priceNew + '</div></div></div></a>')
                         .append('<div id="' + type.dishDto.dishId + '" class="modalDialog"><div><div class="modalImage" index="' + type.dishDto.dishId + '" style="background-image: url(../img/' + type.dishDto.photo + '); background-size: cover;"></div>' +
                         '<div class="modalInfoHeader">' + type.name + '</div><div class="modaldeadline">' + date.getHours() + ':' + date.getMinutes() + '</div>' +
@@ -37,6 +75,19 @@ Events = function () {
             }
         });
     });
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     //var basketSum = 0;
     //click by add to basket
